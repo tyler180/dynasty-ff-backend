3580554448af2e4bdeeaf5a9745198536ea14630
Tyler McLean <tyler_180@hotmail.com>
2026-08-11 16:55:34 -0600

    updated terraform to build lambda and image for it

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "this" {}
+
+data "aws_ecr_authorization_token" "token" {}
+
+module "ff_backend_lambda" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "8.8.0"
+
+  function_name  = local.name
+  description    = "Dynasty Fantasy Football Backend Lambda Function"
+  create_package = false
+
+  image_uri    = module.ff_backend_build.image_uri
+  package_type = "Image"
+
+  tags = data.context_tags.backend.tags
+}
+
+module "ff_backend_build" {
+  source  = "terraform-aws-modules/lambda/aws//modules/docker-build"
+  version = "8.8.0"
+
+  create_ecr_repo = true
+  ecr_repo        = local.name
+  ecr_repo_lifecycle_policy = jsonencode({
+    "rules" : [
+      {
+        "rulePriority" : 1,
+        "description" : "Keep only the last 5 images",
+        "selection" : {
+          "tagStatus" : "any",
+          "countType" : "imageCountMoreThan",
+          "countNumber" : 5
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      }
+    ]
+  })
+
+  use_image_tag = false # If false, sha of the image will be used
+
+  # use_image_tag = true
+  # image_tag   = "2.0"
+
+  source_path = local.source_path
+  platform    = "linux/amd64"
+
+  triggers = {
+    dir_sha = local.dir_sha
+  }
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,84 @@
+terraform {
+  required_version = ">= 1.15.0"
+  backend "s3" {
+    region       = "us-west-2"
+    bucket       = "749rmw-tf-backends"
+    key          = "dynasty-ff/backend.tfstate"
+    encrypt      = true
+    use_lockfile = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.5.0"
+    }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "4.5.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 1.3"
+    }
+    context = {
+      source  = "registry.terraform.io/cloudposse/context"
+      version = "0.5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2" # Update to your preferred region
+}
+
+provider "docker" {
+  registry_auth {
+    address  = format("%v.dkr.ecr.%v.amazonaws.com", data.aws_caller_identity.this.account_id, data.aws_region.current.region)
+    username = data.aws_ecr_authorization_token.token.user_name
+    password = data.aws_ecr_authorization_token.token.password
+  }
+}
+
+provider "context" {
+  delimiter       = "-"
+  enabled         = true
+  tags_key_case   = "title"
+  tags_value_case = "none"
+
+  properties = {
+    namespace = {
+      required        = false
+      include_in_tags = true
+      tags_value_case = "lower"
+    }
+    environment = {
+      required        = true
+      include_in_tags = true
+      tags_value_case = "lower"
+    }
+    stage = {
+      required        = false
+      include_in_tags = true
+      tags_value_case = "lower"
+    }
+    name = {
+      required        = true
+      include_in_tags = true
+      tags_value_case = "lower"
+    }
+  }
+
+  values = {
+    environment = "dev"
+    name        = basename(dirname(path.cwd))
+  }
+}
+
+data "context_tags" "backend" {}
+
+data "context_label" "backend" {
+  template = "${data.context_tags.backend.tags["Name"]}-${data.context_tags.backend.tags["Environment"]}"
+}
+
+data "context_config" "backend" {}
